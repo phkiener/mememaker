32f8c4eb06186b6febc675155e1f7e2965cde137
Philipp Kiener <kiener.phil@gmail.com>
2026-07-12 18:14:05 +0200

    Compile css as well

--- build.js
+++ build.js
@@ -1,17 +1,43 @@
-import { build } from "tsdown";
+import * as ts from "tsdown";
+import * as css from "lightningcss";
 import * as fs from "node:fs";
+import * as asyncfs from "node:fs/promises";
 
-const buildOptions = {
-    entry: ["src/app.ts"],
+const tsBuild = {
+    config: {
+        entry: ["src/app.ts"],
 
-    format: "module",
-    outDir: "app/js",
-    platform: "browser",
+        format: "module",
+        outDir: "app/js",
+        platform: "browser",
 
-    sourcemap: true,
-    minify: true,
+        sourcemap: true,
+        minify: true,
+    },
+    rebuild: async () => {
+        await ts.build(Object.assign({ clean: true, failOnWarn: true }, tsBuild.config));
+    },
+    incrementalBuild: async () => {
+        await ts.build(Object.assign({ clean: false, failOnWarn: false }, tsBuild.config));
+    }
+};
+
+const cssBuild = {
+    config: {
+        filename: "src/app.css",
+        minify: true
+    },
+    rebuild: async () => {
+        const result = await css.bundleAsync(cssBuild.config);
+        await asyncfs.writeFile("app/css/app.css", result.code);
+    },
+    incrementalBuild: async () => {
+        const result = await css.bundleAsync(cssBuild.config);
+        await asyncfs.writeFile("app/css/app.css", result.code);
+    }
 }
 
+const targets = [tsBuild, cssBuild];
 await rebuild();
 
 if (process.argv.includes("--watch")) {
@@ -24,22 +50,17 @@ if (process.argv.includes("--watch")) {
 }
 
 async function incrementalBuild() {
-    const incrementalBuildOptions = Object.assign({ clean: false, failOnWarn: false }, buildOptions);
-
     try {
-        console.log("Building app.ts...");
-        await build(incrementalBuildOptions);
-        console.log("Incremental build done.")
+        for (const target of targets) {
+            await target.incrementalBuild();
+        }
     } catch (error) {
-        console.error("Incremental build failed");
-        console.error(error);
+        console.error("Incremental build failed: ", error);
     }
 }
 
 async function rebuild() {
-    const rebuildOptions = Object.assign({ clean: true, failOnWarn: true }, buildOptions);
-
-    console.log("Rebuilding app.ts...");
-    await build(rebuildOptions);
-    console.log("Rebuild done.")
+    for (const target of targets) {
+        await target.rebuild();
+    }
 }

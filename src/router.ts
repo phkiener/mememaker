213cb38c0641017cf0a29062d0049dfdfb74931d
Philipp Kiener <kiener.phil@gmail.com>
2026-07-26 20:13:33 +0200

    hm

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,11 @@ import { Controller } from "./controller";
 export function resolveController(path: string, query: string): Controller | null {
     const parsedQuery = new URLSearchParams(query);
 
-    if (path === "") {
+    if (path === "" || path === "/") {
         return new IndexController();
     }
 
-    if (path === "caption/" || path === "caption") {
+    if (path === "caption" || path === "caption/") {
         return new CaptionController(parsedQuery.get("id")!);
     }
 

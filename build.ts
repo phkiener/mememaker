7dab596527ca13db83d247597e1f690d89365298
Philipp Kiener <kiener.phil@gmail.com>
2026-07-27 12:19:23 +0200

    Timestamps in build script

--- build.ts
+++ build.ts
@@ -6,13 +6,15 @@ import * as fs from "node:fs";
 import { Target } from "./build/target";
 
 const targets: Target[] = [typescript, styles, assets, pages];
-const incrementalBuildDelay = 2000;
+const incrementalBuildDelay = 1000;
 
+process.stdout.write(`[${new Date().toISOString()}] Starting rebuild...\n`);
 for (const target of targets) {
-    process.stdout.write(`${target.name}...`);
+    process.stdout.write(` - ${target.name}...`);
     await target.build();
     process.stdout.write(` success!\n`);
 }
+process.stdout.write(`[${new Date().toISOString()}] Finished rebuild.\n`);
 
 process.stdout.write("\n");
 
@@ -22,8 +24,9 @@ if (process.argv.includes("--watch")) {
     fs.watch("src/", { recursive: true, }, () => {
         clearTimeout(buildTimer);
         buildTimer = setTimeout(async () => {
+            process.stdout.write(`[${new Date().toISOString()}] Starting incremental build...\n`);
             for (const target of targets) {
-                process.stdout.write(`${target.name}...`);
+                process.stdout.write(` - ${target.name}...`);
                 try {
                     await (target.incrementalBuild ?? target.build)();
                     process.stdout.write(` success!\n`);
@@ -32,7 +35,7 @@ if (process.argv.includes("--watch")) {
                 }
             }
 
-            process.stdout.write("\n");
+            process.stdout.write(`[${new Date().toISOString()}] Finished incremental build.\n`);
         }, incrementalBuildDelay);
     });
 }
